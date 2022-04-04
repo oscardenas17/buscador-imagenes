067bbb09683bbd29f789872538f7673b9755929d
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-04-03 20:48:28 -0500

    Header de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Formik, Form, Field } from "formik";
+import './header.css'
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={ {search: ''}}
+          onSubmit={async values =>{
+            //llamado de api
+            console.log(values)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search'/>
+          </Form>
+
+        </Formik>
       </header>
     </div>
   );
