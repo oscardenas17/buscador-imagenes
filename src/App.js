a349041163f9dc1a6071cd4cc030aaac583203ee
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-04-04 07:58:31 -0500

    Llamando al API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+
 import { Formik, Form, Field } from "formik";
+import { useState } from "react";
 import './header.css'
+
 const App = ()=> {
+  const [photos,setPhotos] = useState([])
+  console.log( {photos})
   return (
     <div>
       <header>
         <Formik
           initialValues={ {search: ''}}
           onSubmit={async values =>{
-            //llamado de api
-            console.log(values)
+            const response = await fetch(`https:/api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+              headers:{
+                'Authorization': 'Client-ID jR_CYouZUjkTaCTRyKFzSrz81ogn6EKCA8xlotdzpZA'
+              }
+            })
+            const data = await response.json()
+            
+            //console.log(data.results)
+            setPhotos(data.results) 
           }}
         >
           <Form>
@@ -22,3 +34,6 @@ const App = ()=> {
 }
 
 export default App;
+
+
+
